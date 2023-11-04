7c5a1ec100820e4de4588f4df817676bc8cbc7ce
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-04 07:19:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,79 @@
 import streamlit as st
-from transformers import pipeline
+from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
 
-# Load the sentiment analysis model
-analyzer = pipeline('sentiment-analysis')
+# List of pre-trained models for users to select
+pretrained_models = {
+    "Sentiment Analysis": "text-sentiment-analysis",
+    "Text Classification (roberta-base)": "text-classification/roberta-base",
+    "Text Classification (distilbert-base)": "text-classification/distilbert-base-cased",
+    "Custom Model": "custom"
+}
 
-# Main function
-def main():
-  # Streamlit UI components
-  st.title("Sentiment Analysis App")
-  # Subtitle showing author name and institution
-  st.markdown("**Dr. Tuan Nguyen-Sy** ")
-  st.markdown("<font color='gray'>Institute for Computational Science and Artificial Intelligence, Van Lang University</font>", unsafe_allow_html=True)
-  
-  user_input = st.text_area("Enter a sentence:", "")
-  
-  # Perform sentiment analysis when the user submits the input
-  if user_input:
-      with st.spinner("Analyzing sentiment..."):
-          # Analyze sentiment for the user input
-          sentiment = analyzer(user_input)
-          predicted_sentiment = sentiment[0]['label']
-          confidence = sentiment[0]['score']
-  
-          # Display the predicted sentiment and confidence
-          st.subheader("Predicted Sentiment:")
-          st.write(predicted_sentiment)
-          st.subheader("Confidence:")
-          st.write(confidence)
-  
-  # Instructions for the user
-  st.markdown(
-      """
-      **Instructions:**
-      - Enter a sentence in the text box.
-      - The app will predict the sentiment of the input sentence.
-      """
-  )
+# Streamlit UI components
+st.title("Hugging Face Transformers App")
 
-# Run the app
-if __name__ == "__main__":
-  main()
+# User input for choosing a pre-trained model or custom model
+selected_model = st.selectbox("Select a pre-trained model:", list(pretrained_models.keys()))
+user_input = st.text_area("Enter a sentence:", "")
+
+if selected_model == "Sentiment Analysis":
+    # Load the sentiment analysis model
+    analyzer = pipeline('sentiment-analysis')
+
+    if user_input:
+        with st.spinner("Analyzing sentiment..."):
+            sentiment = analyzer(user_input)
+            predicted_sentiment = sentiment[0]['label']
+            confidence = sentiment[0]['score']
+
+            st.subheader("Predicted Sentiment:")
+            st.write(predicted_sentiment)
+            st.subheader("Confidence:")
+            st.write(confidence)
+
+elif selected_model.startswith("Text Classification"):
+    # Load the selected text classification model
+    model_name = pretrained_models[selected_model]
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    if user_input:
+        with st.spinner("Classifying text..."):
+            inputs = tokenizer(user_input, return_tensors="pt", padding=True, truncation=True)
+            outputs = model(**inputs)
+            logits = outputs.logits
+            predicted_class = logits.argmax().item()
+
+            st.subheader("Predicted Class (Label ID):")
+            st.write(predicted_class)
+
+else:  # Custom Model
+    # User input for specifying a custom model and tokenizer
+    custom_model_name = st.text_input("Enter a custom model name:", "")
+    custom_tokenizer_name = st.text_input("Enter a custom tokenizer name:", "")
+
+    if custom_model_name and custom_tokenizer_name:
+        # Load the custom model and tokenizer
+        custom_model = AutoModelForSequenceClassification.from_pretrained(custom_model_name)
+        custom_tokenizer = AutoTokenizer.from_pretrained(custom_tokenizer_name)
+
+        if user_input:
+            with st.spinner("Running custom model..."):
+                inputs = custom_tokenizer(user_input, return_tensors="pt", padding=True, truncation=True)
+                outputs = custom_model(**inputs)
+                logits = outputs.logits
+                predicted_class = logits.argmax().item()
+
+                st.subheader("Predicted Class (Label ID):")
+                st.write(predicted_class)
+
+# Instructions for the user
+st.markdown(
+    """
+    **Instructions:**
+    - Select a pre-trained model from the dropdown list.
+    - Enter a sentence in the text box.
+    - The app will perform the selected task based on the chosen model.
+    - For the Custom Model option, you can specify your custom model and tokenizer names.
+    """
+)
