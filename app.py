5598d98fb5972757e0abd1e22a8d0c5903ff58a0
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-04 07:27:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from transformers import pipeline, AutoModelForSequenceClassification, AutoToken
 # List of pre-trained models for users to select
 pretrained_models = {
     "Sentiment Analysis": "text-sentiment-analysis",
-    "Text Classification (roberta-xsmall)": "microsoft/deberta-xsmall",
-    "Text Classification (roberta-large)": "microsoft/deberta-large",
-    "Custom Model": "custom"
+    "Text Classification": "Text Classification"
 }
 
 # Streamlit UI components
-st.title("Hugging Face Transformers App")
+st.title("Social Sentiment Analysis")
+# Subtitle showing author name and institution
+st.markdown("**Dr. Tuan Nguyen-Sy** ")
+st.markdown("<font color='gray'>Institute for Computational Science and Artificial Intelligence, Van Lang University</font>", unsafe_allow_html=True)
 
 # User input for choosing a pre-trained model or custom model
 selected_model = st.selectbox("Select a pre-trained model:", list(pretrained_models.keys()))
@@ -20,53 +21,21 @@ if selected_model == "Sentiment Analysis":
     # Load the sentiment analysis model
     analyzer = pipeline('sentiment-analysis')
 
-    if user_input:
-        with st.spinner("Analyzing sentiment..."):
-            sentiment = analyzer(user_input)
-            predicted_sentiment = sentiment[0]['label']
-            confidence = sentiment[0]['score']
+elif selected_model == "Text Classification":
+    # Load the sentiment analysis model
+    analyzer = pipeline('text-classification')
 
-            st.subheader("Predicted Sentiment:")
-            st.write(predicted_sentiment)
-            st.subheader("Confidence:")
-            st.write(confidence)
+if user_input:
+    with st.spinner("Analyzing sentiment..."):
+        sentiment = analyzer(user_input)
+        predicted_sentiment = sentiment[0]['label']
+        confidence = sentiment[0]['score']
 
-elif selected_model.startswith("Text Classification"):
-    # Load the selected text classification model
-    model_name = pretrained_models[selected_model]
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    if user_input:
-        with st.spinner("Classifying text..."):
-            inputs = tokenizer(user_input, return_tensors="pt", padding=True, truncation=True)
-            outputs = model(**inputs)
-            logits = outputs.logits
-            predicted_class = logits.argmax().item()
-
-            st.subheader("Predicted Class (Label ID):")
-            st.write(predicted_class)
-
-else:  # Custom Model
-    # User input for specifying a custom model and tokenizer
-    custom_model_name = st.text_input("Enter a custom model name:", "")
-    custom_tokenizer_name = st.text_input("Enter a custom tokenizer name:", "")
-
-    if custom_model_name and custom_tokenizer_name:
-        # Load the custom model and tokenizer
-        custom_model = AutoModelForSequenceClassification.from_pretrained(custom_model_name)
-        custom_tokenizer = AutoTokenizer.from_pretrained(custom_tokenizer_name)
-
-        if user_input:
-            with st.spinner("Running custom model..."):
-                inputs = custom_tokenizer(user_input, return_tensors="pt", padding=True, truncation=True)
-                outputs = custom_model(**inputs)
-                logits = outputs.logits
-                predicted_class = logits.argmax().item()
-
-                st.subheader("Predicted Class (Label ID):")
-                st.write(predicted_class)
-
+        st.subheader("Predicted Sentiment:")
+        st.write(predicted_sentiment)
+        st.subheader("Confidence:")
+        st.write(confidence)
+        
 # Instructions for the user
 st.markdown(
     """
